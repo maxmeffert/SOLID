6b87e649d6b7100a5c38dd8262e58c567854275b
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2019-06-18 01:45:28 +0200

    added comment

--- TheLofSOLID/Examples/LSP.Examples.CSharp/Example01/ShapeClient.cs
+++ TheLofSOLID/Examples/LSP.Examples.CSharp/Example01/ShapeClient.cs
@@ -2,7 +2,7 @@ namespace Example01
 {
     public class ShapeClient
     {
-        // DrawShape cannot be used with all (possible) subtypes of Shape.
+        // DrawShape does not have the same behavior for all (possible) subtypes of Shape, consider Triangle.
         // Shape is not a super type regarding the Draw-behavior, hence, Liskov-/"Behavioral"-substitution is impossible.
         // Also, this violates the Open/Closed Principle.
         // It's just not a good method ;-) 
